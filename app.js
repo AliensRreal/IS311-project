9cbc1cb8c680d8f6b0adb61a1f262a832458b2c5
Anmar <anythingexample118@gmail.com>
2024-11-18 01:49:49 +0300

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "sequel",
+  password: "",
   database: "student_club",
 });
 //------------
