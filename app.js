0c6c634836e3dc03c70c1d932b00ad706e77d9a0
itsWafii <itswafii@gmail.com>
2024-11-17 21:31:16 +0300

    Members manipulation

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const app = express();
 //------------
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "sequel",
+  password: "WSQL2018",
   database: "student_club",
 });
 //------------
